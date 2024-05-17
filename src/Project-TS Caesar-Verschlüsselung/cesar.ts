b04c4f601482a8f1b81636a1957b81576325b2ff
pablobeckg <beckgarreaud@gmail.com>
2024-05-17 19:34:54 +0200

    committ

---
+++ src/Project-TS Caesar-Verschlüsselung/cesar.ts
@@ -0,0 +1,49 @@
+const getText = document.getElementById('secretText') as HTMLInputElement;
+const getInput = document.getElementById('secretNumber') as HTMLInputElement;
+const getEncodeBttn = document.getElementById('encoder');
+const getDecodeBttn = document.getElementById('decoder');
+const getSolution = document.querySelector('h3')
+
+function encode(text:string, num: number) {
+    const split = text.toUpperCase().split('');
+    console.log(split);
+    const newArray: number[]  = [];
+    const newArray2: string[] = [];
+    split.forEach(element => {
+        if(element == ' ') {
+            newArray.push(element.charCodeAt(0));
+        } else {
+            newArray.push(element.charCodeAt(0) + num);
+        }
+        
+    });
+    for (let index = 0; index < newArray.length; index++) {
+        
+        if(newArray[index] > 90) {
+            newArray[index] -= 26
+        } 
+        
+    }
+    newArray.forEach(element => {
+        const toLetter = String.fromCharCode(element)
+        newArray2.push(toLetter)
+    });
+
+    return newArray2.join('')
+}
+
+getEncodeBttn?.addEventListener('click', (event: Event) => {
+    event.preventDefault();
+    if(getSolution) {
+        getSolution.textContent = encode(getText.value, parseInt(getInput.value));
+    }
+    
+})
+getDecodeBttn?.addEventListener('click', (event: Event) => {
+    event.preventDefault();
+    if(getSolution) {
+        getSolution.textContent = getText.value;
+    }
+    
+})
+
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,3 @@
 import './style.css'
-import './01_Enum_Set_Map_Tuple/Enum-TS-Level-3_1/3_1'
+// import './01_Enum_Set_Map_Tuple/Enum-TS-Level-3_1/3_1'
+import './Project-TS Caesar-Verschlüsselung/cesar'
